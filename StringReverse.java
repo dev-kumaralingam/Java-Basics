2d918abd838e2d0118e8d181fe9011ca6ce1bf5f
dev-kumaralingam <kumaralingam2384@gmail.com>
2024-08-05 19:30:47 +0530

    Fifteenth commit

---
+++ StringReverse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class StringReverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str=sc.nextLine();
+        sc.close();
+        for(int i=str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+        System.out.println();
+    }
+}
