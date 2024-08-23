04756d6f6a7cabf71fec5ffda7632ca3e25d9516
dev-kumaralingam <kumaralingam2384@gmail.com>
2024-08-23 19:44:49 +0530

    Twentyfirst commit

---
+++ SumThreeNumbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SumThreeNumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a first value: ");
+        int v1=sc.nextInt();
+        System.out.println("Enter a second value: ");
+        int v2=sc.nextInt();
+        System.out.println("Enter a third value: ");
+        int v3=sc.nextInt();
+        sc.close();
+        v1=v1+v2+v3;
+        System.out.println("Addition: "+v1);
+       
+    }
+} 
