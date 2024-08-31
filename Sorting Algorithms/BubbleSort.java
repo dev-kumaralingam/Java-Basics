5f6026eac94c06e423ef36cb909a6e5a51b2bed4
dev-kumaralingam <kumaralingam2384@gmail.com>
2024-08-31 19:54:18 +0530

    Twentyeightth commit

---
+++ Sorting Algorithms/BubbleSort.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        sc.close();
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        bubbleSort(array);
+
+        System.out.println("Sorted Array:");
+        printArray(array);
+    }
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
