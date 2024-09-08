48d8c611e9d95c1ccfb2e83c197629ff3f433f30
dev-kumaralingam <kumaralingam2384@gmail.com>
2024-09-08 19:46:01 +0530

    Thirtyfifth commit

---
+++ Sorting Algorithms/ShellSort.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ShellSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        shellSort(array);
+
+        System.out.println("Sorted Array:");
+        printArray(array);
+    }
+
+    public static void shellSort(int[] array) {
+        int n = array.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = array[i];
+                int j;
+                for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                    array[j] = array[j - gap];
+                }
+                array[j] = temp;
+            }
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
+
