b84d87e32d3a3f2fd6f15e12a1694d0f777daea5
dev-kumaralingam <kumaralingam2384@gmail.com>
2024-09-16 18:57:25 +0530

    Thirtnineth commit

---
+++ CountSpace.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class CountSpace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = sc.nextLine();
+        sc.close();
+        
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        System.out.println("Space Count: " + count);
+    }
+}
